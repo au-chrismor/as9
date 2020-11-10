3369562ff87f1acf2c644621b813d9a0d53a0c9c
Christopher Moran <christopher.moran@versent.com.au>
2020-11-10 20:44:03 +1100

    Make output.c more ANSI-Like

--- output.c
+++ output.c
@@ -1,9 +1,9 @@
+#include "asdefs.h"
+
 /*
  *  stable --- prints the symbol table in alphabetical order
  */
-stable(ptr)
-
-struct nlist *ptr;
+void stable(struct nlist *ptr)
 {
   if (ptr != NULL)
     {
@@ -15,9 +15,7 @@ struct nlist *ptr;
 /*
  *  cross  --  prints the cross reference table
  */
-cross(point)
-
-struct nlist *point;
+void cross(struct nlist *point)
 {
 struct link *tp;
 int i = 1;
